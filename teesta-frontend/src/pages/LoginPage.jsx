8178ffdc581ceb6d7d99353c71a4f0b68b69f347
ChoncholKumarRay <fusitivechonchol@gmail.com>
2025-01-04 09:03:38 +0600

    admin login page frontend setup

---
+++ teesta-frontend/src/pages/LoginPage.jsx
@@ -0,0 +1,86 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
+import "./styles/LoginPage.css";
+
+const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordVisible, setPasswordVisible] = useState(false);
+  const [errorMessage, setErrorMessage] = useState(""); // State for error messages
+
+  const navigate = useNavigate();
+
+  const togglePasswordVisibility = () => {
+    setPasswordVisible((prevState) => !prevState);
+  };
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch(
+        "http://localhost:5000/api/admin-login", // API endpoint
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ username, password }),
+        }
+      );
+
+      const data = await response.json();
+
+      if (response.ok) {
+        navigate(data.redirectTo);
+      } else {
+        setErrorMessage(data.message); // Set error message to display
+      }
+    } catch (error) {
+      console.error("Error during login:", error);
+      setErrorMessage("An error occurred. Please try again.");
+    }
+  };
+
+  return (
+    <div className="login-page">
+      <form className="login-form" onSubmit={handleLogin}>
+        <h2 className="form-title">Admin Login</h2>
+        <div className="form-username">
+          <input
+            type="text"
+            placeholder="Username"
+            className="form-input"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+          />
+        </div>
+        <div className="password-field">
+          <input
+            type={passwordVisible ? "text" : "password"}
+            placeholder="Password"
+            className="form-input"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+          <button
+            type="button"
+            className="toggle-password"
+            onClick={togglePasswordVisibility}
+          >
+            {passwordVisible ? <FaEye /> : <FaEyeSlash />}
+          </button>
+        </div>
+        {errorMessage && <div className="error-message">{errorMessage}</div>}
+        <button type="submit" className="login-button">
+          Log In
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
